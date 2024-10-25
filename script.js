66a57ba5a20779c9bf49e606f27e1f4f0aa3b8e3
Bartelus <wilroed@gmail.com>
2024-10-25 12:07:14 +0200

    Favoritt knapp konsoll logger

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello!');
+let alleBildeKort = document.querySelectorAll(".kort");
+
+for (let bildeKort of alleBildeKort){
+    let favorittKnapp = bildeKort.querySelector("button");
+    console.log(bildeKort);
+    console.log(favorittKnapp);
+
+ favorittKnapp.addEventListener("click", () => sorterFavoritter (bildeKort)); //* kjører denne forhåndsdefinerte funksjonen, men med en egen variable for dette tilfellet *//  
+
+}
+
+function sorterFavoritter(kort) {
+console.log(kort);
+}
