bf944402d081b38fa0330154e42c05ade990d569
Bartelus <wilroed@gmail.com>
2024-10-25 13:06:10 +0200

    create image object

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
+//* Hent kortene fra HTML *//
 let alleBildeKort = document.querySelectorAll(".kort");
 
-for (let bildeKort of alleBildeKort){
+
+//* For hvert av kortene, legg til en funksjon
+for (let bildeKort of alleBildeKort) {
+    //* her henter vi ut knappene
     let favorittKnapp = bildeKort.querySelector("button");
     console.log(bildeKort);
     console.log(favorittKnapp);
 
- favorittKnapp.addEventListener("click", () => sorterFavoritter (bildeKort)); //* kjører denne forhåndsdefinerte funksjonen, men med en egen variable for dette tilfellet *//  
-
+ //* kjører denne forhåndsdefinerte funksjonen ved klikk, men med en egen variable for dette tilfellet *//  
+ favorittKnapp.addEventListener("click", () => sorterFavoritter (bildeKort)); 
 }
-
+/**
+* Favoritt knapp funksjon
+* @param {Element} kort //* gir kort beskrivelse som element, som gir den en liste med kode knyttet til seg der den blir referert til
+*/
 function sorterFavoritter(kort) {
 console.log(kort);
+
+
+
+// Hent ut url og bilde navn
+
+let bilde = kort.querySelector ("img");
+
+
+// lag bilde objektet
+let bildeObjekt = {
+    url: bilde.src,
+    name: bilde.alt,
+}
+
+console.log(bildeObjekt);
+
+console.log(bilde.src);
+console.log(bilde.alt);
 }
